7a51306f2843effef2debe449ab50ac61a878085
Felix Wu <fwu@shipserv.com>
2025-02-23 15:07:17 +0000

    Add complex example route

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        complexExample: 'complexExample.html',
+      },
+    },
+  },
 })
